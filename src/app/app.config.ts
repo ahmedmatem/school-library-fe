216ce939dc7add94e5f628f287b69f1d966bc620
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-26 23:05:26 +0200

    Add msal - microsoft authentication library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,39 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import {
+  MsalModule,
+  MSAL_INSTANCE,
+  MSAL_GUARD_CONFIG,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalInterceptor,
+  MsalService,
+  MsalGuard,
+  MsalBroadcastService
+} from '@azure/msal-angular';
+import {
+  msalInstanceFactory,
+  msalGuardConfigFactory,
+  msalInterceptorConfigFactory
+} from './auth/msal.factories';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(MsalModule),
+    { provide: MSAL_INSTANCE, useFactory: msalInstanceFactory },
+    { provide: MSAL_GUARD_CONFIG, useFactory: msalGuardConfigFactory },
+    { provide: MSAL_INTERCEPTOR_CONFIG, useFactory: msalInterceptorConfigFactory },
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
   ]
 };
