8e1b307c92ebca5598675780d5a382327acdfe3d
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-30 15:53:00 +0200

    Add CompleteProfile component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,9 +9,14 @@ export const routes: Routes = [
     { path: 'catalog', component: CatalogComponent },
     { path: 'resources/:id', component: ResourceDetailsComponent },
     { path: 'my', component: MyLibraryComponent },
-    { 
+    {
         path: 'teacher',
         loadChildren: () => import('./features/teacher/teacher.routes').then(m => m.TEACHER_ROUTES)
+    },
+    {
+        path: 'complete-profile',
+        loadComponent: () => import('./features/complete-profile/complete-profile.component')
+            .then(m => m.CompleteProfileComponent)
     },
     { path: '**', redirectTo: '' }
 ];
---
+++ src/app/features/complete-profile/complete-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complete-profile',
+  imports: [],
+  templateUrl: './complete-profile.component.html',
+  styleUrl: './complete-profile.component.css',
+})
+export class CompleteProfileComponent {
+
+}
