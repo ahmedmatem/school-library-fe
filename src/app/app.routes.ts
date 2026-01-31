1f3ba8c71b05459a4cf5ca1ee1be78bb368e276e
Ahmed Matem <ahmedmatem@gmail.com>
2026-01-31 16:37:33 +0200

    Admin admin route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './features/home/home.component';
 import { CatalogComponent } from './features/catalog/catalog.component';
 import { MyLibraryComponent } from './features/my-library/my-library.component';
 import { ResourceDetailsComponent } from './features/resource-details/resource-details.component';
+import { AdminUsersComponent } from './features/admin/users/admin-users.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
     { path: 'resources/:id', component: ResourceDetailsComponent },
     { path: 'my', component: MyLibraryComponent },
+    { path: 'admin', component: AdminUsersComponent },
     {
         path: 'teacher',
         loadChildren: () => import('./features/teacher/teacher.routes').then(m => m.TEACHER_ROUTES)
